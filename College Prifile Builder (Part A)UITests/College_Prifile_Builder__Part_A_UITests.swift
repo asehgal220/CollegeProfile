4ac30c108c611a7f1b234494f117cffeb2ceecfa
Ananya Sehgal <asehgal@ananyas-macbook-air.local>
2017-06-19 12:49:14 -0500

    Initial Commit

---
+++ College Prifile Builder (Part A)UITests/College_Prifile_Builder__Part_A_UITests.swift
@@ -0,0 +1,36 @@
+//
+//  College_Prifile_Builder__Part_A_UITests.swift
+//  College Prifile Builder (Part A)UITests
+//
+//  Created by Ananya Sehgal on 6/19/17.
+//  Copyright © 2017 The Dawg. All rights reserved.
+//
+
+import XCTest
+
+class College_Prifile_Builder__Part_A_UITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
